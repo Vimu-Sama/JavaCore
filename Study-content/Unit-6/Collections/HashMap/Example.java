e5c95c7730d85cd97f4aa43d97d655463b8847ef
Vimarsh Sharma <vimarshsharmabsr2000@gmail.com>
2026-05-07 09:00:37 +0530

    .

---
+++ Study-content/Unit-6/Collections/HashMap/Example.java
@@ -0,0 +1,27 @@
+
+import java.util.HashMap;
+
+public class Example {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>() ;
+        hashMap.put(null, "Arnav") ;
+        hashMap.put(1, "Harsh") ;
+        hashMap.put(2, "Krishna") ;
+
+        // values() traversal
+        for(var val: hashMap.values()){
+            System.out.println("HashMap values-> "+ val) ;
+        }
+
+        // keySet() traversal
+        for(var key: hashMap.keySet()){
+            System.out.println("HashMap keys-> " + key);
+        }
+
+        // entrySet() traversal
+        for(var map: hashMap.entrySet()){
+            System.out.println("HashMap sets-> " + map);
+        }
+
+    }    
+}
